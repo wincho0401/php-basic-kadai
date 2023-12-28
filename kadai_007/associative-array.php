8ee23e1354715f71e3c80e9943030b1d3808913f
wincho0401 <152471956+wincho0401@users.noreply.github.com>
2023-12-29 06:26:10 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+<body>
+    <p>
+        <?php
+            $shopping = ['name' => 'onion', 'price' => '200', 'weight' => '160' ];
+
+            print_r($shopping);
+ 
+        ?>
+    </p>
+</body>
+</html>
